1c6af908911ade932fe296b167738f64b5f94a58
Viktor Nikitin <vnikitin@anl.gov>
2021-08-24 00:04:49 -0500

    add streams

--- src/lprec/solver.py
+++ src/lprec/solver.py
@@ -5,14 +5,21 @@ from lprec.cfunc import cfunc
 from lprec.utils import *
 import cupy as cp
 from cupyx.scipy.fft import rfft, irfft, rfft2, irfft2
+import cupyx
+import dxchange
+import threading
+import queue
+import h5py
+import time
+
+pinned_memory_pool = cp.cuda.PinnedMemoryPool()
+cp.cuda.set_pinned_memory_allocator(pinned_memory_pool.malloc)
 
 # 'float32'- c++ code needs to be recompiled with changed directives in cfunc.cuh
 mtype = 'float16'
-
-
 class LpRec(cfunc):
 
-    def __init__(self, n, nproj, nz):
+    def __init__(self, n, nproj, nz, ndark, nflat, data_type):
 
         # precompute parameters for the lp method
         self.Pgl = initsgl.create_gl(n, nproj)
@@ -42,6 +49,16 @@ class LpRec(cfunc):
         super().setgrids(fZnptr, lp2p1, lp2p2, lp2p1w, lp2p2w,
                          C2lp1, C2lp2, lpids, wids, cids,
                          nlpids, nwids, ncids)
+        self.ndark = ndark
+        self.nflat = nflat
+        self.data_type = data_type
+                    
+        self.write_threads = []
+        self.data_queue = queue.Queue()   
+        self.data_gpu_queue = queue.Queue(maxsize=1)   
+        self.obj_gpu_queue = queue.Queue(maxsize=1)   
+        self.running = True
+            
 
     def fbp_filter(self, data):
         """FBP filtering of projections"""
@@ -52,7 +69,9 @@ class LpRec(cfunc):
 
     def darkflat_correction(self, data, dark, flat):
         """Dark-flat field correction"""
-        data = (data-dark)/cp.maximum(flat-dark, 1e-6)
+        dark0 = cp.mean(dark,axis=0).astype(mtype)
+        flat0 = cp.mean(flat,axis=0).astype(mtype)                
+        data = (data.astype(mtype)-dark0)/cp.maximum(flat0-dark0, 1e-6)
         return data
 
     def minus_log(self, data):
@@ -66,32 +85,10 @@ class LpRec(cfunc):
         data[cp.isinf(data)] = 0
         return data
 
-    def binning(self, data, bin_level):
-        """Downsample data"""
-        for k in range(bin_level):
-            data = 0.5*(data[..., ::2, :]+data[..., 1::2, :])
-            data = 0.5*(data[..., :, ::2]+data[..., :, 1::2])
-        return data
 
-    def gpu_copy(self, data,  dark, flat,  theta, ids, bin_level):
-        """Copy data to GPU as mtype arrays"""
-        data_gpu = cp.zeros([self.nproj,self.nz,self.n],dtype=mtype)
-        dark_gpu = cp.zeros([self.nz,self.n],dtype=mtype)
-        flat_gpu = cp.ones([self.nz,self.n],dtype=mtype)
-        
-        data_gpu[:,:len(ids)] = cp.array(data[:, ids])
-        # data_gpu = self.binning(data_gpu, bin_level)
-        dark_gpu[:len(ids)] = cp.mean(cp.array(dark[:, ids]), axis=0)
-        # dark_gpu = self.binning(dark_gpu, bin_level)
-        flat_gpu[:len(ids)] = cp.mean(cp.array(flat[:, ids]), axis=0)
-        # flat_gpu = self.binning(flat_gpu, bin_level)                
-        theta_gpu = cp.array(theta).astype(mtype)
-        
-        return data_gpu, dark_gpu, flat_gpu, theta_gpu
-
-    def recon(self, data, dark, flat, theta):
+    def recon(self, el):
         """Full reconstruction pipeline for a data chunk"""
-        data = self.darkflat_correction(data, dark, flat)
+        data = self.darkflat_correction(el['data'], el['dark'], el['flat'])
         data = self.minus_log(data)        
         data = self.fix_inf_nan(data)        
         data = self.fbp_filter(data)        
@@ -99,50 +96,152 @@ class LpRec(cfunc):
         data = cp.ascontiguousarray(data.swapaxes(0, 1))
         obj = cp.zeros([self.nz, self.n, self.n], dtype=mtype)
         self.backprojection(obj.data.ptr, data.data.ptr)
-        
-        # import matplotlib.pyplot as plt
-        # obj2=self.backprojection2(data)
-        # plt.subplot(131)
-        # plt.imshow(obj[0].astype('float32').get(),cmap='gray')
-        # plt.colorbar()
-        # plt.subplot(132)
-        # plt.imshow(obj2[0].astype('float32').get(),cmap='gray')
-        # plt.colorbar()
-        # plt.subplot(133)
-        # dif = np.abs(obj2[0].astype('float32').get()-obj[0].astype('float32').get())
-        # plt.imshow(dif,cmap='gray')
-        # print(np.linalg.norm(dif))
-        # plt.clim([0,dif.max()/10,])
-        # plt.colorbar()
-        #
-        # plt.show()
-
+                
         return obj
-
-    def recon_all(self, data, flat, dark, theta, bin_level=0, pchunk=16):
-        """Reconstruction by splitting data into chunks"""
-        obj = np.zeros([data.shape[1],data.shape[2],data.shape[2]],dtype=mtype)        
+            
+    def pinned_array(self,array):
+        # first constructing pinned memory
+        mem = cp.cuda.alloc_pinned_memory(array.nbytes)
+        src = np.frombuffer(
+                    mem, array.dtype, array.size).reshape(array.shape)
+        src[...] = array
+        return src
+    
+    def thread0(self, file_name):
+        fid = h5py.File(file_name, 'r')
+        data = fid['exchange/data']# why <=4 doesnt work???
+        flat = fid['exchange/data_white']
+        dark = fid['exchange/data_dark']
+        theta = fid['exchange/theta']
         for ids in chunk(range(data.shape[1]), self.nz):
-            print(f'Reconstruction of slices {ids[0]}..{ids[-1]}')
-            # tic()
-            # thread for cpu-gpu copy
-            tic()
-            data_gpu, dark_gpu, flat_gpu, theta_gpu = self.gpu_copy(
-                data, dark, flat, theta, ids,  bin_level)
-            cp.cuda.stream.get_current_stream().synchronize()
-            print(f'copy to GPU {toc():.2f}')
-            # thread for processing
-            tic()
-            obj_gpu = self.recon(data_gpu, dark_gpu, flat_gpu, theta_gpu)
-            cp.cuda.stream.get_current_stream().synchronize()
-            print(f'recon {toc():.2f}')
-            tic()
-            obj[ids] = obj_gpu[:len(ids)].get()
-            cp.cuda.stream.get_current_stream().synchronize()
-            print(f'copy back to CPU {toc():.2f}')
-            # print(f'Time {toc():.3f}s')
+            # print(f'0: {ids[0]} {ids[-1]}')
+            el = {}
+            el['data'] = data[:,ids]
+            el['flat'] = flat[:,ids]
+            el['dark'] = dark[:,ids]
+            el['theta'] = theta
+            el['ids'] = ids          
+            self.data_queue.put(el)  
+                        
+    def thread1(self):
+        stream1 = cp.cuda.Stream(non_blocking=False)
+        el = {}
+        el['data'] = np.zeros([self.nproj, self.nz,self.n],dtype=self.data_type)
+        el['dark'] = np.zeros([self.ndark, self.nz,self.n],dtype=self.data_type)
+        el['flat'] = np.ones([self.nflat, self.nz,self.n],dtype=self.data_type)
         
-        return obj
+        
+        el_pinned = {}
+        el_pinned['data'] = self.pinned_array(el['data'])
+        el_pinned['dark'] = self.pinned_array(el['dark'])
+        el_pinned['flat'] = self.pinned_array(el['flat'])
+        while self.running:
+            el = self.data_queue.get()    
+            el_pinned['data'][:,:len(el['ids'])] = el['data']
+            el_pinned['dark'][:,:len(el['ids'])] = el['dark']
+            el_pinned['flat'][:,:len(el['ids'])] = el['flat']
+            el_gpu = {}
+            el_gpu['data'] = cp.zeros([self.nproj, self.nz,self.n],dtype=self.data_type)
+            el_gpu['dark'] = cp.zeros([self.ndark, self.nz,self.n],dtype=self.data_type)
+            el_gpu['flat'] = cp.ones([self.nflat, self.nz,self.n],dtype=self.data_type)
+                            
+            with stream1: 
+                el_gpu['data'].set(el_pinned['data'])      
+                el_gpu['dark'].set(el_pinned['dark'])      
+                el_gpu['flat'].set(el_pinned['flat'])                  
+                stream1.synchronize()
+            # cp.cuda.stream.get_current_stream().synchronize()
+                
+            el_gpu['ids'] = el['ids'].copy()
+            
+            # print(f"1: {el_gpu['ids'][0]} {el_gpu['ids'][-1]}")
+            self.data_gpu_queue.put(el_gpu)              
+            if(el_gpu['ids'][-1]==899):
+                print('1 done')
+                return             
+    
+    def thread2(self):
+        stream2 = cp.cuda.Stream(non_blocking=False)        
+            
+        while self.running:
+            el_gpu = self.data_gpu_queue.get()            
+            elo_gpu = {}
+            with stream2:
+                elo_gpu['obj'] = self.recon(el_gpu) 
+            stream2.synchronize()
+            elo_gpu['ids'] = el_gpu['ids'].copy()
+            # print(f"2: {elo_gpu['ids'][0]} {elo_gpu['ids'][-1]}")
+            self.obj_gpu_queue.put(elo_gpu)   
+            if(elo_gpu['ids'][-1]==899):
+                return             
+
+    
+    def thread3(self):        
+            stream3 = cp.cuda.Stream(non_blocking=False)        
+            obj_pinned = self.pinned_array(np.zeros([self.nz,self.n, self.n],dtype=mtype))
+            while self.running:
+                with stream3:                
+                    elo_gpu = self.obj_gpu_queue.get()                
+                    # print(f"3: {elo_gpu['ids'][0]} {elo_gpu['ids'][-1]}")                
+                    elo_gpu['obj'].get(out=obj_pinned)                    
+                # cp.cuda.stream.get_current_stream().synchronize()
+                    obj = obj_pinned.copy()# temp
+                    obj.dtype = 'uint16'#???temp
+                stream3.synchronize()
+                
+                write_thread = threading.Thread(target=dxchange.write_tiff_stack,
+                                            args = (obj[:len(elo_gpu['ids'])],),
+                                            kwargs = {'fname': '/local/ssd/lprec/r',
+                                                        'start': elo_gpu['ids'][0],
+                                                        'overwrite': True})                    
+                write_thread.start()
+                self.write_threads.append(write_thread)                
+                if(elo_gpu['ids'][-1]==899):
+                    return        
+        
+    def recon_all(self, data, flat, dark, theta, pchunk=16):
+        """Reconstruction by splitting data into chunks"""
+        
+        thread0 = threading.Thread(target=self.thread0,args = ('/local/ssd/286_2_spfp_019.h5',))   
+        thread1 = threading.Thread(target=self.thread1)   
+        thread2 = threading.Thread(target=self.thread2)   
+        thread3 = threading.Thread(target=self.thread3)   
+        thread0.start()                
+        thread1.start()
+        thread2.start()
+        thread3.start()
+        thread0.join()        
+        thread1.join()        
+        thread2.join()        
+        thread3.join()        
+        
+        # thread3.join()                     
+        
+        for thread in self.write_threads:
+            thread.join()     
+        # while self.running or not self.data_queue.empty():
+        #     #tic()                            
+        #     el = self.data_queue.get()    
+        #     el_gpu[v%2] = self.gpu_copy(el)      
+        #     if(v>1):
+        #         obj_gpu[v%2] = self.recon(el_gpu[(v-1)%2])            
+        #     if(v>2):
+        #         obj = obj_gpu[(v-2)%2,:len(el['ids'])].get()
+        #         obj.dtype = 'uint16'#???
+        #         write_thread = threading.Thread(target=dxchange.write_tiff_stack,
+        #                                     args = (obj,),
+        #                                     kwargs = {'fname': '/local/ssd/lprec/r',
+        #                                                 'start': el['ids'][0],
+        #                                                 'overwrite': True})            
+        #         if write_thread is not None:
+        #             write_thread.start()
+        #             write_threads.append(write_thread)
+        #     v+=1
+        # print(f'waiting threads')
+        # for thread in write_threads:
+        #     thread.join()                
+        # print(f'waiting threads done')            
+                
 
     # def minterplp(self, f, g, x, y, xi, ni, mi):
     #     xc = x.astype('int32')
--- tests/test_file.py
+++ tests/test_file.py
@@ -3,22 +3,23 @@ from lprec.timing import *
 import h5py
 import dxchange
 
-[Nslices,Nproj,N] = [16,1500,2448] # 32 for RTX4000
+[nslices,nproj,n] = [16,1500,2448] # 32 for RTX4000
+[ndark,nflat] = [40,80]
+data_type = 'uint8'
 
 fid = h5py.File('/local/ssd/286_2_spfp_019.h5', 'r')
-data = fid['exchange/data'][:,:]# why <=4 doesnt work???
-flat = fid['exchange/data_white'][:,:]
-dark = fid['exchange/data_dark'][:,:]
-theta = fid['exchange/theta'][:]
+data = fid['exchange/data']# why <=4 doesnt work???
+flat = fid['exchange/data_white']
+dark = fid['exchange/data_dark']
+theta = fid['exchange/theta']
 
 tic()
-clpthandle = lprec.LpRec(N, Nproj, Nslices)
+clpthandle = lprec.LpRec(n, nproj, nslices, ndark, nflat, data_type)
 print(toc())
 tic()
-obj = clpthandle.recon_all(data,flat,dark,theta)
+clpthandle.recon_all(data,flat,dark,theta)
 print(f'Total time for reconstruction:{toc():.3f}s')
-print(f'Saving tiffs...')
-tic()
-# dxchange.write_tiff_stack(obj.astype(
-    # 'float32'), '/local/ssd/lprec/r', start=0, overwrite=True)
-print(f'Save tiffs time: {toc():.3f}s')
+# print(f'Saving tiffs...')
+# tic()
+# # dxchange.write_tiff_stack(obj.astype('float32'), '/local/ssd/lprec/r', start=0, overwrite=True)
+# print(f'Save tiffs time: {toc():.3f}s')
